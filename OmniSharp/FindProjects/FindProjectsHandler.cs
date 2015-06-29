0f28efb46e3802f4156bf5cc0c1a707070d74a52
jtbm37 <jean@tbm.email>
2015-06-30 00:12:24 +0100

    Fixed merge issue

--- OmniSharp/FindProjects/FindProjectsHandler.cs
+++ OmniSharp/FindProjects/FindProjectsHandler.cs
@@ -8,32 +8,12 @@ namespace OmniSharp.FindProjects
     public class FindProjectsHandler
     {
         readonly ISolution _solution;
-<<<<<<< HEAD
-=======
 
->>>>>>> upstream
         public FindProjectsHandler(ISolution solution)
         {
             _solution = solution;
         }
 
-<<<<<<< HEAD
-        public QuickFixResponse FindAllProjects()
-        {
-            
-            var quickfixes = _solution.Projects
-                .Where(x => x.Title != OrphanProject.ProjectFileName)
-                .OrderBy(x => x.Title)
-                .Select(t => new QuickFix
-                {
-                    Text = t.Title,
-                    FileName = t.FileName
-                });
-
-            return new QuickFixResponse(quickfixes);
-        }
-        
-=======
         public FindProjectsResponse FindAllProjects()
         {
             var projects = _solution.Projects
@@ -42,6 +22,5 @@ namespace OmniSharp.FindProjects
                 .Select(t => new MsBuildProject(t));
             return new FindProjectsResponse { MSBuild = new MsBuildWorkspaceInformation{ Projects = projects }};
         }
->>>>>>> upstream
     }
 }
